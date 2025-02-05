4de2a133b921add2d418951df3be91e15d3865cb
iana.sergieieva <iana.sergieieva@bahnhof.net>
2025-02-05 12:15:34 -0800

    gh-pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '//budgetapp-frontend/',
+  base: '/budgetapp-frontend/',
   build: {
     outDir: 'dist',
   },
